21f53eb90f677ed93a6412bc70ec977a1ceb467a
Sxllaire <space.shundere@gmail.com>
2025-10-01 15:02:42 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,46 @@
+// *************************************************************************** 
+// 
+//   Your Name 
+//   Your Harper ID
+//   Your Course and Section Number 
+// 
+//   I certify that this is my own work and where appropriate an extension 
+//   of the starter code provided for the assignment. 
+// ***************************************************************************
+
+#include <iostream>
+
+int main() {
+    int size;
+
+    // user prompt
+    std::cout << "Enter the number of elements: ";
+    std::cin >> size;
+
+    // Validation
+    if (size <= 0) {
+        std::cerr << "Invalid array size.\n";
+        return 1;
+    }
+
+    int* data = new int[size];
+
+    // Input
+    std::cout << "Enter " << size << " integers:\n";
+    for (int i = 0; i < size; ++i) {
+        std::cout << "Value " << i + 1 << ": ";
+        std::cin >> data[i];
+    }
+
+    // Outputs
+    std::cout << "\nArray output using array indexing:\n";
+    for (int i = 0; i < size; ++i) {
+        std::cout << "data[" << i << "] = " << data[i] << "\n";
+    }
+    std::cout << "\nArray output using pointer arithmetic:\n";
+    for (int i = 0; i < size; ++i) {
+        std::cout << "*(data + " << i << ") = " << *(data + i) << "\n";
+    }
+    delete[] data;
+    return 0;
+}
